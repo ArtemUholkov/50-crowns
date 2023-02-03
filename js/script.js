2675e84d6b14776aebca44f2d4de7e61f382d125
Artem <artem.uholkov@ukr.net>
2023-02-03 15:39:33 +0200

    header links ready

--- js/script.js
+++ js/script.js
@@ -40,26 +40,15 @@ function myFunction() {
     window.location.href = 'thankyou.html';
   }, 2000);
 }
-
-const faders = document.querySelectorAll('.fade-in');
-const sliders = document.querySelectorAll('.slide-in');
-const appearOptions = { threshold: 0, rootMargin: '0px 0px -150px 0px' };
-
-const appearOnScroll = new IntersectionObserver(function (entries, appearOnScroll) {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting) {
-      return;
-    } else {
-      entry.target.classList.add('appear');
-      appearOnScroll.unobserve(entry.target);
-    }
-  });
-}, appearOptions);
-
-faders.forEach((fader) => {
-  appearOnScroll.observe(fader);
-});
-
-sliders.forEach((slider) => {
-  appearOnScroll.observe(slider);
-});
+function scrollToBrands() {
+  document.querySelector('#brands').scrollIntoView(true);
+}
+function scrollToAdvantages() {
+  document.querySelector('#advantages').scrollIntoView(true);
+}
+function scrollToTestimonials() {
+  document.querySelector('#testimonials').scrollIntoView(true);
+}
+function scrollToContacts() {
+  document.querySelector('#contacts').scrollIntoView(true);
+}
